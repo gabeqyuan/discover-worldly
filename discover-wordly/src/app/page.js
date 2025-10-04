d83f9c400e32a8d36deb0998c255f9e94e09b084
Patrick Fish <patrickfish10@icloud.com>
2025-10-04 16:40:45 -0400

    .

--- discover-wordly/src/app/page.js
+++ discover-wordly/src/app/page.js
@@ -3,7 +3,7 @@ import MapClientWrapper from "./components/MapClientWrapper";
 export default function Home() {
   return (
     // Server component: renders layout and client wrapper for interactive parts
-    <div className="overflow-hidden">
+    <div>
       <MapClientWrapper />
     </div>
   );
